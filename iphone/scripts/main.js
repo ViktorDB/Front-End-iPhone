f35d15a3dc2f50cdb68156d0908792b40b7dd4b5
ViktorDB <Viktor@Viktors-MacBook-Pro.local>
2015-01-06 12:48:14 +0100

    Cross-Browser fixes / image compressing

--- iphone/scripts/main.js
+++ iphone/scripts/main.js
@@ -367,6 +367,18 @@ $(document).ready(function() {
 
 
 
+function msieversion() {
 
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer, return version number
+        $( "#compare-wrapper" ).css( "top", "120px" );
+    else                 // If another browser, return 0
+
+    return false;
+}
+
+msieversion();
 
 
